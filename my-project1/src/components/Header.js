92c910778273e36419318ffe1f202bcfc7ddbbab
mithuran2002 <mithuran64@gmail.com>
2025-03-30 19:00:51 +0530

    header changes addes

--- my-project1/src/components/Header.js
+++ my-project1/src/components/Header.js
@@ -3,21 +3,19 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-  const token = localStorage.getItem("token"); // Check if user is logged in
+  const token = localStorage.getItem("token");
 
   const handleLogout = () => {
-    localStorage.removeItem("token"); // Clear token on logout
-    localStorage.removeItem("user"); // Clear user data if stored
-    navigate("/login"); // Redirect to login page
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
   };
 
   return (
     <header className="bg-white shadow-md py-4 px-6 flex justify-between">
-      <h1 className="text-xl font-bold text-blue-700">Serene  Tours</h1>
+      <h1 className="text-xl font-bold text-blue-700">Rent Nest</h1>
       <nav>
       <Link to="/home" className="mr-4">Home</Link>
-
-        <Link to="/tours" className="mr-4">Tours</Link>
 
         {token ? (
           <>
